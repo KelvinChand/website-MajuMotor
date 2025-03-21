cef6fb57d6ed4adc3b37cce2dbad293000bc3a8f
Kelvin <kelvin.william@mhs.mdp.ac.id>
2025-03-21 19:19:39 +0700

    initial commit - 21/03/2025

---
+++ database/migrations/2025_03_21_113925_create_penjualans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penjualans', function (Blueprint $table) {
+            $table->uuid('idPenjualan')
+            ->primary();
+            $table->double('total_harga');
+            $table->string('status',10)->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualans');
+    }
+};
---
+++ database/migrations/2025_03_21_113952_create_penjualanproduks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penjualanproduks', function (Blueprint $table) {
+            $table->uuid('idPenjualanProduk')
+            ->primary();
+            $table->double('jumlah');
+            $table->integer('kuantitas');
+            $table->foreignUuid('idPenjualan')
+                ->references('idPenjualan')
+                ->on('penjualans')
+                ->onDelete('cascade');
+            $table->foreignUuid('idProduk')
+                ->references('idProduk')
+                ->on('produks')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualanproduks');
+    }
+};
